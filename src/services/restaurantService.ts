19d3aac3bdd4ecbf68c981c9acbb0802c6c5144a
Emil <emil.salmi@hotmail.se>
2023-03-13 09:17:43 +0100

    made a variable of the api

--- src/services/restaurantService.ts
+++ src/services/restaurantService.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { IBooking } from "../models/IBooking";
 
+const BASE_URL = "https://school-restaurant-api.azurewebsites.net/";
+
 export const callApi = async(): Promise <IBooking[]> => {
     let response = await axios.get<IBooking[]>(
-        "https://school-restaurant-api.azurewebsites.net/"
+        BASE_URL
     );
 
     return response.data
