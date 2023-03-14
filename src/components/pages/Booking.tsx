11988c0debd5b2adbe4e76846c3d51f8799ccbbf
NatasaIc <iconomou.ng@gmail.com>
2023-03-14 13:06:42 +0100

    changed color on calender

--- src/components/pages/Booking.tsx
+++ src/components/pages/Booking.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import { BookingCalendar } from '../BookingCalendar';
 import { BookingHeading, BookingContainer } from '../../styles/BookingPage.style';
+import { Navbar } from './Navbar';
 
 export const Booking = () => {
 	return (
 		// TEST CODE
+		<>
 		<BookingContainer>
 			<BookingHeading >Booking</BookingHeading>
 			<BookingCalendar />
@@ -12,5 +14,6 @@ export const Booking = () => {
 			<img src='' alt=''/>
 			</>
 		</BookingContainer>
+		</>
 	);
 };
