bd5d971125f295ce8788c08c2203d383f00b479e
mcsahlin <103230661+mcsahlin@users.noreply.github.com>
2023-03-15 19:15:10 +0100

    created interface in bookingcontext

---
+++ src/contexts/BookingContext.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+interface INewBookingContext {
+	restaurantId: string;
+	date: string;
+	time: string;
+	numberOfGuests: number;
+	customer: {
+		name: string;
+		lastname: string;
+		email: string;
+		phone: string;
+	};
+}
+const NewBookingContext = React.createContext<INewBookingContext>({});
