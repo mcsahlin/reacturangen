79d25d137ea472418c06d158a078119760801b34
mcsahlin <103230661+mcsahlin@users.noreply.github.com>
2023-03-15 19:39:21 +0100

    fixed imports

--- src/components/HomeSecondBackground.tsx
+++ src/components/HomeSecondBackground.tsx
@@ -1,16 +1,19 @@
-import MeatOnPlate from "../assets/img/meat-2.jpeg"
-import { H2 } from "../styles/FrontHeader"
-import { ImageSecondWrapper, Wrapper } from "../styles/Wrappers"
+import MeatOnPlate from '../assets/img/meat-2.jpeg';
+import { H2 } from './styled/FrontHeader';
+import { ImageSecondWrapper, Wrapper } from './styled/Wrappers';
 
 export const HomeSecondBackground = () => {
-    return(
-        <>
-            <Wrapper>
-                <div>
-                    <ImageSecondWrapper src={MeatOnPlate} alt="man cooking meat"></ImageSecondWrapper>
-                    <H2>REACTAURANGEN</H2>
-                </div>
-            </Wrapper>
-        </>
-    )
-}
+	return (
+		<>
+			<Wrapper>
+				<div>
+					<ImageSecondWrapper
+						src={MeatOnPlate}
+						alt='man cooking meat'
+					></ImageSecondWrapper>
+					<H2>REACTAURANGEN</H2>
+				</div>
+			</Wrapper>
+		</>
+	);
+};
--- src/components/HomeVideoBackground.tsx
+++ src/components/HomeVideoBackground.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import HomeVideo from "../assets/video/new-cutting-video.mp4";
-import { H1 } from '../styles/FrontHeader';
-import { VideoWrapper, Wrapper } from '../styles/Wrappers';
-
+import HomeVideo from '../assets/video/new-cutting-video.mp4';
+import { H1 } from './styled/FrontHeader';
+import { VideoWrapper, Wrapper } from './styled/Wrappers';
 
 export const HomeVideoBackground = () => {
-
-    return (
-        <>
-            <Wrapper>
-                <H1>REACTAURANGEN</H1>
-                <VideoWrapper autoPlay loop muted playsInline preload="auto" >
-                    <source src={HomeVideo} type="video/mp4"/>
-                    Your browser does not support HTML5 videos
-                </VideoWrapper>
-            </Wrapper>
-        </>
-    )
-}
-
-
-
+	return (
+		<>
+			<Wrapper>
+				<H1>REACTAURANGEN</H1>
+				<VideoWrapper autoPlay loop muted playsInline preload='auto'>
+					<source src={HomeVideo} type='video/mp4' />
+					Your browser does not support HTML5 videos
+				</VideoWrapper>
+			</Wrapper>
+		</>
+	);
+};
