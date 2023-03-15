ace319fb211988e7047f3b0a54b2916a58af1244
mcsahlin <103230661+mcsahlin@users.noreply.github.com>
2023-03-15 16:36:24 +0100

    added contact hero img and styled component

---
+++ src/components/styled/ContactHero.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import contactHeroImg from '../../assets/img/meat-1.jpg';
+
+export const HeroSection = styled.section`
+	background-image: url(${contactHeroImg});
+	background-size: cover;
+	height: 500px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const InfoBox = styled.div`
+	background-color: black;
+	color: white;
+`;
