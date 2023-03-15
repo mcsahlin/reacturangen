04daef2018ff83057c8f900b37fe447f02938fe7
emilgsalmi <emil.salmi@hotmail.se>
2023-03-15 22:50:52 +0100

    responstivity done

--- src/components/styled/Wrappers.tsx
+++ src/components/styled/Wrappers.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Meat from "../../assets/img/meat-2.jpeg"
 
 export const ContactInfoWrapper = styled.article`
 	display: flex;
@@ -24,6 +25,9 @@ export const InfoTimeWrapper = styled.div`
 export const Wrapper = styled.div`
 	display: flex;
 	position: relative;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 	width: 100%;
 	height: 100%;
 
@@ -31,9 +35,18 @@ export const Wrapper = styled.div`
 	}
 `;
 
+export const TestWrapper = styled.div`
+	
+`;
+
 export const VideoWrapper = styled.video`
-	height: 100%;
+	height: 50vh;
 	width: 100%;
+	object-fit: cover;
+
+	@media (min-width: 700px) {
+		height: 90vh;
+	}
 `;
 
 export const InfoWrapper = styled(Wrapper)`
@@ -69,10 +82,32 @@ export const ImageInfoWrapper = styled.img`
 	}
 `;
 
-export const HourWrapper = styled(Wrapper)``;
-
-export const ImageSecondWrapper = styled.img`
-	height: 100%;
+export const ImageBackgroundWrapper = styled(Wrapper)`
+	background-image: url(${Meat});
+	background-size: cover;
+	height: 40vh;
 	width: 100%;
 	clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+
+	@media (min-width: 700px) {
+		height: 90vh;
+	}
 `;
+
+export const FontWrapper = styled.div`
+	height: 100%;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: whitesmoke;
+	
+
+	@media (min-width: 700px) {
+		align-items: center;
+		justify-content: center;
+		font-size: x-large;
+	}
+`
+
+
