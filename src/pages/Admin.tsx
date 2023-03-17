4156f1020db8474658d1bc017039a04a21d4205a
NatasaIc <iconomou.gmail.com>
2023-03-17 06:40:46 +0100

    admin page with a table that we can administer the bookings

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,11 +1,46 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { AdminContainer, AdminTable, AdminThead, AdminTr, ThId, NameTh, LastNameTh, DateTh, TimeTh, GuestsTh, AdimnBody, BodyTr, BodyTd, DeleteIcon } from '../components/styled/Admin.style';
+import { ICustomer } from '../models/ICustomer';
+import AiFillDelete from 'react-icons';
+
+// interface Props {
+// cutomer: ICustomer,
+// }
+
 export const Admin = () => {
 	const [admin, setAdmin] = useState(false);
+
+	// const handleDelete = (id: number) => {
+
+	// }
+
 	return (
 		// TEST CODE
 		<>
-			<p>Admin</p>
+		<AdminContainer>
+		<h2>Bokningar</h2>
+			<AdminTable>
+				<AdminThead>
+					<AdminTr>
+					<ThId>#</ThId>
+					<NameTh>Namn</NameTh>
+					<LastNameTh>Efternamn</LastNameTh>
+					<DateTh>Datum</DateTh>
+					<TimeTh>Tid</TimeTh>
+					<GuestsTh>Antal</GuestsTh>
+					</AdminTr>
+				</AdminThead>
+				<AdimnBody>
+					<BodyTr>
+						<BodyTd>{}</BodyTd>
+						{/* <DeleteIcon onClick={() => handleDelete()}>
+						<AiFillDelete />
+						</DeleteIcon> */}
+					</BodyTr>
+				</AdimnBody>
+			</AdminTable>
+		</AdminContainer>
 		</>
 	);
+	return <BodyTd>{}</BodyTd>
 };
