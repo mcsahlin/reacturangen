0fd7675a010c45c9033a4dc68c95d59528200f5b
NatasaIc <iconomou.gmail.com>
2023-08-01 21:13:41 +0200

    added bookingView

--- src/components/Admin/bookingView.tsx
+++ src/components/Admin/bookingView.tsx
@@ -1,3 +1,89 @@
-const BookingInfo = () => {};
+import { IBookingProps } from "../../models/IBookingProps";
+import { useEffect, useState, ChangeEvent, FormEvent } from "react";
+import { ICustomer } from "../../models/ICustomer";
+import { get_bookings, get_customers } from "../../services/restaurantService";
+import { Booking } from "../Admin/bookingModel";
+import { IBooking } from "../../models/IBooking";
 
-export const Bookings = () => {};
+const BookingInfo = (props: IBookingProps) => {
+  const [findbooking, setFindBooking] = useState<ICustomer>();
+  useEffect(() => {
+    const getData = async () => {
+      let bookingInfo = await get_customers(props.booking.date);
+      setFindBooking(bookingInfo[0]);
+    };
+    if (findbooking) return;
+    getData();
+  });
+  return <>{BookingInfo}</>;
+};
+
+export const Bookings = () => {
+  const [bookings, setBookings] = useState<IBooking[]>([]);
+  const [filterbookings, setFilterBookings] = useState<IBooking[]>([]);
+  const [findbooking, setFindBooking] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    let filteredBookings = bookings.filter((x) => x.date === findbooking);
+    setFilterBookings(filteredBookings);
+    console.log(findbooking);
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      let allBookings = await get_bookings();
+      setBookings(allBookings);
+      setFilterBookings(allBookings);
+    };
+
+    if (bookings.length > 0) return;
+    getData();
+  }, []);
+  const handleclick = () => {
+    setFilterBookings(bookings);
+    setFindBooking("");
+  };
+
+  let bookingsHtml = filterbookings.map((booking) => {
+    return <Booking booking={booking} key={booking._id} />;
+  });
+
+  return (
+    <section>
+      <form id="searchForm" onSubmit={handleSubmit}>
+        <p>Sök bokning</p>
+        <div>
+          <input
+            type="text"
+            id="findbooking"
+            value={findbooking}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setFindBooking(e.target.value);
+            }}
+          />
+
+          <button>Sök</button>
+        </div>
+      </form>
+      <button onClick={handleclick}>Visa alla</button>
+      <div id="bookingContainer"></div>
+      <table>
+        <thead>
+          <tr>
+            <th>Bokning ID</th>
+            <th>Datum</th>
+            <th>Tid</th>
+            <th>Gäster</th>
+            <th>ID</th>
+            <th>Förnamn</th>
+            <th>Efternamn</th>
+            <th>E-mail</th>
+            <th>Telefon</th>
+          </tr>
+        </thead>
+        <tbody>{bookingsHtml}</tbody>
+      </table>
+    </section>
+  );
+};
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,46 +1,59 @@
-import { useState } from 'react';
-import { AdminContainer, AdminTable, AdminThead, AdminTr, Th, AdimnBody, BodyTr, BodyTd, DeleteIcon } from '../components/styled/Admin.style';
-import { ICustomer } from '../models/ICustomer';
-import AiFillDelete from 'react-icons';
+import { useState } from "react";
+import {
+  AdminContainer,
+  AdminTable,
+  AdminThead,
+  AdminTr,
+  Th,
+  AdimnBody,
+  BodyTr,
+  BodyTd,
+  DeleteIcon,
+} from "../components/styled/Admin.style";
+import { ICustomer } from "../models/ICustomer";
+import AiFillDelete from "react-icons";
+import Booking from "./Booking";
+import { Bookings } from "../components/Admin/bookingView";
 
 // interface Props {
 // cutomer: ICustomer,
 // }
 
 export const Admin = () => {
-	const [admin, setAdmin] = useState(false);
+  const [admin, setAdmin] = useState(false);
 
-	// const handleDelete = (id: number) => {
+  // const handleDelete = (id: number) => {
 
-	// }
+  // }
 
-	return (
-		// TEST CODE
-		<>
-		<AdminContainer>
-		<h2>Bokningar</h2>
-			<AdminTable>
-				<AdminThead>
-					<AdminTr>
-					<Th>#</Th>
-					<Th>Namn</Th>
-					<Th>Efternamn</Th>
-					<Th>Datum</Th>
-					<Th>Tid</Th>
-					<Th>Antal</Th>
-					</AdminTr>
-				</AdminThead>
-				<AdimnBody>
-					<BodyTr>
-						<BodyTd>{}</BodyTd>
-						{/* <DeleteIcon onClick={() => handleDelete()}>
+  return (
+    // TEST CODE
+    <>
+      <Bookings />
+      <AdminContainer>
+        <h2>Bokningar</h2>
+        <AdminTable>
+          <AdminThead>
+            <AdminTr>
+              <Th>#</Th>
+              <Th>Namn</Th>
+              <Th>Efternamn</Th>
+              <Th>Datum</Th>
+              <Th>Tid</Th>
+              <Th>Antal</Th>
+            </AdminTr>
+          </AdminThead>
+          <AdimnBody>
+            <BodyTr>
+              <BodyTd>{}</BodyTd>
+              {/* <DeleteIcon onClick={() => handleDelete()}>
 						<AiFillDelete />
 						</DeleteIcon> */}
-					</BodyTr>
-				</AdimnBody>
-			</AdminTable>
-		</AdminContainer>
-		</>
-	);
-	return <BodyTd>{}</BodyTd>
+            </BodyTr>
+          </AdimnBody>
+        </AdminTable>
+      </AdminContainer>
+    </>
+  );
+  return <BodyTd>{}</BodyTd>;
 };
